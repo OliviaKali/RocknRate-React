796e3dcc9cf0ec04631dc57ad0a5dcdffc59e070
JChesh <sfuchesh@hotmail.com>
2019-08-29 12:29:23 -0400

    New

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const passport = require("passport");
 const SpotifyStrategy = require('passport-spotify').Strategy;
 var path = require('path');
 
-var apiRoutes = require("./app/routes/apiRoutes")
+// var apiRoutes = require("./app/routes/apiRoutes")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./app/models/blog");
+// var db = require("./app/models/blog");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +18,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Static directory
-app.use(express.static("app/public"));
+// app.use(express.static("app/public"));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -29,15 +29,15 @@ passport.deserializeUser(function(obj, done) {
 });
 
 // Routes
-require("./app/routes/html-routes.js")(app);
-require("./app/routes/api-blogRoutes.js")(app);
-require('./app/routes/artist-api-routes.js')(app);
+// require("./app/routes/html-routes.js")(app);
+// require("./app/routes/api-blogRoutes.js")(app);
+// require('./app/routes/artist-api-routes.js')(app);
 
-app.use(apiRoutes)
+// app.use(apiRoutes)
 
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: false }).then(function() {
+// db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+// });
