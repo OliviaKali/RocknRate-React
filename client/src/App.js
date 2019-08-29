c6ba088f891893d3cbdd3e662061bb945078ff45
OKali <o.kalinowski27@gmail.com>
2019-08-29 12:59:16 -0400

    connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/index";
-
-import Blog from "./components/pages/Blog";
+import Blog from "./pages/blog";
+import About from "./pages/about";
 
 function App() {
   return (
     <Router>
     <div>
-      <Route exact path="/" component={Home} />
-        <Route path="/artist" component={} />
-        <Route exact path="/about" component={} />
+        <Route exact path="/" component={Home} />
+        <Route path="/artist" component={Blog} />
+        <Route exact path="/about" component={About} />
+        
     </div>
     </Router>
   );
