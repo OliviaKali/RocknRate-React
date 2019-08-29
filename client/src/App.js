9edafa00480892f720b9ddb542dc3ae836f97ecb
OKali <o.kalinowski27@gmail.com>
2019-08-29 12:22:54 -0400

    Added router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import Home from "./pages/index";
+
+import Blog from "./components/pages/Blog";
 
 function App() {
   return (
+    <Router>
     <div>
-     
+      <Route exact path="/" component={Home} />
+        <Route path="/artist" component={} />
+        <Route exact path="/about" component={} />
     </div>
+    </Router>
   );
 }
 
