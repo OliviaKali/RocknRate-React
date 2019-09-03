1cb81bf721af8cf09f4924c01a0edbeea84e3e29
JChesh <sfuchesh@hotmail.com>
2019-09-03 11:41:57 -0400

    new

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/index";
 import Blog from "./pages/blog";
 import About from "./pages/about";
+import PrimarySearchAppBar from "./components/NavBar/navbar";
 
 function App() {
   return (
-    <Router>
     <div>
+      <PrimarySearchAppBar />
+      <Router>
         <Route exact path="/" component={Home} />
         <Route path="/artist" component={Blog} />
         <Route exact path="/about" component={About} />
-        
+      </Router>
     </div>
-    </Router>
 
   );
 }
