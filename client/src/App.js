74fd67c4bfeae0282686943db2811c4e8324fe05
OKali <o.kalinowski27@gmail.com>
2019-09-07 22:50:51 -0400

    clear

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,8 @@ class App extends Component {
   handleFormSubmit = event => {
     event.preventDefault();
     this.searchArtist(this.state.searchTerm);
-    this.getBlogEntries(this.state.searchTerm)
+    this.getBlogEntries(this.state.searchTerm);
+    this.setState({searchTerm: ""});
   };
 
   searchArtist = search => {
