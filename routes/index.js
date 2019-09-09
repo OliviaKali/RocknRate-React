99e32a5c30143a7d73d42ace92b87cd930ac62bb
OKali <o.kalinowski27@gmail.com>
2019-09-09 16:01:58 -0400

    heroku routes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-app.get('*', (request, response) => {
+router.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
