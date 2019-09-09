6e9aacd74758bc1de7c15261fb7dd617c776f33a
OKali <o.kalinowski27@gmail.com>
2019-09-09 16:19:31 -0400

    routes heroku

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,18 @@ router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
+// }
+
+// else {
+//   app.use(express.static(path.join(__dirname, '/client/public')));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/public/index.html"));
+//   });
+// }
+
 module.exports = router;
--- server.js
+++ server.js
@@ -15,12 +15,22 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 const routes = require("./routes/api");
-const routes1 = require("./routes/index")
+// const routes1 = require("./routes/index")
 app.use(routes);
-app.use(routes1);
+// app.use(routes1);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://root:password1@ds031571.mlab.com:31571/heroku_x1c5cqgx");
 
